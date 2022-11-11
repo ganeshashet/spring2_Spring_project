5f77edc2edb2a940679d7236f6f42f42f3b75da0
Ganesha c <93332434+ganeshashet@users.noreply.github.com>
2022-11-11 15:23:28 +0530

    Sprint 2 project with Spring and Mysql

---
+++ src/main/java/com/hms/dto/roomDTO.java
@@ -0,0 +1,23 @@
+package com.hms.dto;
+
+import javax.validation.constraints.NotNull;
+
+import com.hms.model.room;
+import com.hms.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class roomDTO {
+
+	@NotNull(message="room id cannot be null")
+	private int roomId;
+	@NotNull(message="room name cannot be null")
+	private String roomName;
+	@NotNull(message="room type cannot be null")
+	private String roomType;
+	
+}
